8976b1ebc688696934d1b9a4c39ff7509b879437
Emery Tate <tateejt@gmail.com>
2020-03-14 01:54:57 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,12 @@
 // add code snippets from README
+let state = { count: 0}
+
+function reducer(state, action){
+  switch (action.type)
+{
+case 'INCREASE_COUNT': 
+  return {count: state.count + 1};
+  default: 
+  return state;
+  }
+}
